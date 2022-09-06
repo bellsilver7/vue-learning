ec048bab4dab4a96f36ec7a80d3e06dca7618c32
bellsilver7 <bellsilver7@gmail.com>
2022-09-07 07:45:43 +0900

    Component Responsibility

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -1,8 +1,10 @@
 import Assignment from "./Assignment.js";
+import AssignmentTags from "./AssignmentTags.js";
 
 export default {
   components: {
     Assignment,
+    AssignmentTags,
   },
 
   template: `
@@ -11,20 +13,19 @@ export default {
         {{ title }}
         <span>({{ assignments.length }})</span>
       </h2>
-      <div class="flex gap-2">
-        <button 
-          @click="currentTag = tag"
-          v-for="tag in tags" 
-          class="border rounded px-1 py-px text-xs"
-          :class="{
-            'border-blue-500 text-blue-500': currentTag === tag,
-          }"
-        >
-          {{ tag }}
-        </button>
-      </div>
+
+      <assignment-tags 
+        :initial-tags="assignments.map((a) => a.tag)" 
+        :current-tag="currentTag"
+        @change="currentTag = $event" 
+      />
+
       <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
-        <assignment v-for="assignment in filteredAssignments" :key="assignment.id" :assignment="assignment"></assignment>
+        <assignment 
+          v-for="assignment in filteredAssignments" 
+          :key="assignment.id" 
+          :assignment="assignment"
+        />
       </ul>
     </section>
   `,
@@ -44,9 +45,6 @@ export default {
     filteredAssignments() {
       if (this.currentTag === "all") return this.assignments;
       return this.assignments.filter((a) => a.tag === this.currentTag);
-    },
-    tags() {
-      return ["all", ...new Set(this.assignments.map((a) => a.tag))];
     },
   },
 };
