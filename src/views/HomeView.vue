b41cd76ee3d46f0ad8d3c807a10745caeb305a4c
bellsilver7 <bellsilver7@gmail.com>
2022-09-10 06:53:23 +0900

    composable: localStorage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,25 @@
 <script setup>
-import TheWelcome from "../components/TheWelcome.vue";
+import { useStorage } from "@/composables/useStorage.js";
+
+let food = useStorage("food");
+let age = useStorage("age");
+
+let obj = useStorage("obj", { one: "two" });
+
+setTimeout(() => {
+  obj.value.one = "CHANGED";
+}, 3000);
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <p>
+      What is your favorite food?
+      <input type="text" v-model="food" />
+    </p>
+    <p>
+      How old are you?
+      <input type="text" v-model="age" />
+    </p>
   </main>
 </template>
